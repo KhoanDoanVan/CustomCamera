e69174ebe04b9d6fd25a153f7a698df0ce59ad94
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-09-21 10:55:36 +0700

    Initial Commit

---
+++ IOS-CustomCamera/IOS_CustomCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_CustomCameraApp.swift
+//  IOS-CustomCamera
+//
+//  Created by Đoàn Văn Khoan on 21/9/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_CustomCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
